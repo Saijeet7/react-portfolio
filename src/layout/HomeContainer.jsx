b77edb101239c108617dd78bcc31fbbb3ddadd7d
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-12-13 22:58:57 +0545

    Complete : minor setup

---
+++ src/layout/HomeContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomeContainer = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default HomeContainer;
+
+const Container = styled.div`
+  width: min(1210px, 100%);
+  padding-left: 20px;
+  padding-right: 20px;
+  box-sizing: border-box;
+  margin: 0 auto;
+  display: grid;
+  grid-template-column: 370px calc(100% - 370px - 100px);
+  gap: 100px;
+`;
