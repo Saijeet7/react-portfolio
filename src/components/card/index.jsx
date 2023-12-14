5753d517acab758f4c983906add51031fe1bb1a9
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-12-14 22:51:39 +0545

    Complete : card icons part

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { CardWrapper } from './Card.styles'
-import CardTop from './CardTop'
+import React from "react";
+import { CardWrapper } from "./Card.styles";
+import CardTop from "./CardTop";
+import CardImage from "./CardImage";
+import CardContent from "./CardContent";
+import CardContact from './CardContact';
 
 const Card = () => {
   return (
     <>
-        <CardWrapper>
-            <CardTop/>
-            
-        </CardWrapper>
+      <CardWrapper>
+        <CardTop />
+        <CardImage />
+        <CardContent />
+        <CardContact/>
+      </CardWrapper>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
